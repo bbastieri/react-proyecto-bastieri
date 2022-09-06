b7343b420d8e3f639f0d0072744bb8d9b9be6bc3
b.bastieri <b.bastieri@live.com>
2022-09-06 16:06:16 -0300

    agrego prop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
+import ItemListContainer from './containers/ItemListContainer';
 
 const App = () => {
+  const message = "Bienvenidx a mi e-commerce";
+   
   return (
     <>
     <Header />
+    <ItemListContainer greeting ={message}/>
     </>
     );
 };
