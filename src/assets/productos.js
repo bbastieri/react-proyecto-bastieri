344cdb60ea6d0e6e60e4593812a5caa0eb2c7b08
b.bastieri <b.bastieri@live.com>
2022-09-13 00:17:04 -0300

    agrego Item, ItemList, Array Productos y Toastify

---
+++ src/assets/productos.js
@@ -0,0 +1,38 @@
+export const productos = [
+    {
+        id:1, 
+        image: "",
+        name: "Body Dominatrix",
+        category: "Bodys",
+        description: "Body latex negro con escote enlazado",
+        price: 4500,
+        stock: 5   
+    },
+    {
+        id:2,
+        image:"",
+        name: "Bombi Heavy Metal",
+        category: "Bombis",
+        description: "Bombacha de red tiro alto",
+        price: 1700,
+        stock: 10
+    },
+    {
+        id:3,
+        image: "",
+        name: "Corpiño Heavy Metal",
+        category: "Corpiños",
+        description: "Corpiño tipo top de lycra engomada combinado con red color negro",
+        price: 3000,
+        stock: 8
+    },
+    {
+        id:4,
+        image:"",
+        name: "Medias Ying Yang Black",
+        category: "Medias",
+        description: "Medias de algodón negras",
+        price: 800,
+        stock: 5
+    }
+]
--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./ItemCount.css"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ItemCount = ({ inicio, stock, onAdd }) =>{
 
@@ -9,13 +11,26 @@ const ItemCount = ({ inicio, stock, onAdd }) =>{
 
     const restar = () => contador > inicio && setContador (contador -1);
 
+    const agregar = () => toast.success("Agregaste tu producto!");
+
     return(
         <>
             <div className="itemListContainer">        
             <button onClick={restar}>-</button>
             <h2>{contador}</h2>
             <button onClick={sumar}>+</button>
-            <button>Agregar al carrito</button>
+            <button onClick={agregar}>Agregar al carrito</button>
+            <ToastContainer 
+                position="bottom-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
             </div>
         </>
     )
