63938596390252889e2a3661ddfaf3bb382c4e3b
b.bastieri <b.bastieri@live.com>
2022-10-27 21:55:36 -0300

    validación form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom"
 import { useCartContext } from "../../context/CartContext";
 import { db } from "../../firebase/firebase"
 import './Form.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const Form = () =>{
@@ -19,6 +21,7 @@ const Form = () =>{
     }
 
     const finalizarCompra = ()=>{
+        if (userData.Nombre !== "" && userData.Apellido !== "" && userData.Email === userData.VerifyEmail) {
         const salesColl = collection(db, "sales");
         addDoc(salesColl, {
             comprador: userData,
@@ -31,10 +34,35 @@ const Form = () =>{
             carrito.forEach(products => {
                 updateStock(products);
             });
+
+            toast('Felicitaciones! Tu compra se realizó con éxito!', {
+                    position: "bottom-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                    });
+            })
+
             setTimeout(()=>{
                 clearCart();    
             },1000)
-        })
+        } else {
+            toast('Oops! Por favor verificá tus datos para continuar', {
+                    position: "bottom-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                })
+        }    
+        
     }
 
     const updateStock = (products) => {
@@ -47,16 +75,19 @@ return (
 <div>
     <div class="ContainerForm">
         <h3>Antes de terminar, por favor ingresá tus datos</h3>
-        <form onSubmit={finalizarCompra} >
+        <form>
            <div className="container-form">
            <div> 
-            <input type="text" placeholder="Nombre"  name="Nombre" onChange={inputChange} required={true}></input>
+            <input type="text" placeholder="Nombre"  name="Nombre" onChange={inputChange}></input>
            </div> 
            <div>
-            <input type="text" placeholder="Apellido"  name="Apellido" onChange={inputChange} required={true}></input>
+            <input type="text" placeholder="Apellido"  name="Apellido" onChange={inputChange}></input>
            </div>
            <div>
-            <input type="text" placeholder="Email"  name="Email" onChange={inputChange} required={true}></input>
+            <input type="text" placeholder="Email"  name="Email" onChange={inputChange}></input>
+           </div>
+           <div>
+            <input type="text" placeholder="Verificá tu email"  name="VerifyEmail" onChange={inputChange}></input>
            </div>
            </div>
         </form>
@@ -64,8 +95,25 @@ return (
            <button type="Submit" onClick={finalizarCompra}>Enviar</button>
            <Link className="Link-Home"to="/"><button>Cancelar</button></Link>
         </div>
-        <p>El código de verificación de tu compra es:</p>
-        <h3 class="salesId"> {salesId} </h3>
+        <div>
+            <p>El código de verificación de tu compra es:</p>
+            <h3 class="salesId"> {salesId} </h3>
+            <p>Guardalo para poder coordinar la entrega!</p>
+        </div>
+        <ToastContainer
+            position="bottom-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+        />
+
+
     </div>
 </div>
 )   
